a3a5cadb56ced71a3f870ab53901513e48614586
Yamini <yamini@iastate.edu>
2022-05-22 23:38:49 -0500

    Updated Behaviour class

--- udrl_one.py
+++ udrl_one.py
@@ -23,33 +23,8 @@ warnings.filterwarnings("ignore")
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 env = GraphWrapper()
 
-# state_size = env.observation_space.shape[0]
-# action_size = env.action_space.n
-# print('State size: {}'.format(state_size))
-# print('Action size: {}'.format(action_size))
-
-# Helper function to create episodes as namedtuple
-make_episode = namedtuple('Episode', 
-                          field_names=['states', 
-                                       'actions', 
-                                       'rewards', 
-                                       'init_command', 
-                                       'total_return', 
-                                       'length', 
-                                       ])
 ### Sample Exploratory Commands
 def sample_command(buffer, last_few):
-    '''Sample a exploratory command
-    
-    Params:
-        buffer (ReplayBuffer)
-        last_few:
-            how many episodes we're gonna look at to calculate 
-            the desired return and horizon.
-    
-    Returns:
-        List of float -- command
-    '''
     if len(buffer) == 0: return [1, 1]
     
     # 1.
@@ -67,20 +42,7 @@ def sample_command(buffer, last_few):
     return [desired_return, desired_horizon]
 
 ### Generate Episode
-
 def generate_episode(env, policy, init_command=[1, 1]):
-    '''
-    Generate an episode using the Behaviour function.
-    
-    Params:
-        env (OpenAI Gym Environment)
-        policy (func)
-        init_command (List of float) -- default [1, 1]
-    
-    Returns:
-        Namedtuple (states, actions, rewards, init_command, total_return, length)
-    '''
-    
     command = init_command.copy()
     desired_return = command[0]
     desired_horizon = command[1]
@@ -100,22 +62,6 @@ def generate_episode(env, policy, init_command=[1, 1]):
         action = policy(state_input, command_input)
         next_state, reward, done, _ = env.step(action)
         
-        # Modifying a bit the reward function punishing the agent, -100, 
-        # if it reaches hyperparam max_steps. The reason I'm doing this 
-        # is because I noticed that the agent tends to gather points by 
-        # landing the spaceshipt and getting out and back in the landing 
-        # area over and over again, never switching off the engines. 
-        # The longer it does that the more reward it gathers. Later on in 
-        # the training it realizes that it can get more points by turning 
-        # off the engines, but takes more epochs to get to that conclusion.
-        if not done and time_steps > max_steps:
-            done = True
-            reward = max_steps_reward
-        
-        # Sparse rewards. Cumulative reward is delayed until the end of each episode
-#         total_rewards += reward
-#         reward = total_rewards if done else 0.0
-        
         states.append(state)
         actions.append(action)
         rewards.append(reward)
@@ -131,22 +77,9 @@ def generate_episode(env, policy, init_command=[1, 1]):
         command = [desired_return, desired_horizon]
         time_steps += 1
         
-    return make_episode(states, actions, rewards, init_command, sum(rewards), time_steps)
+    return (states, actions, rewards, init_command, sum(rewards), time_steps)
 
 def generate_episodes(env, behavior, buffer, n_episodes, last_few):
-    '''
-    1. Sample exploratory commands based on replay buffer
-    2. Generate episodes using Algorithm 2 and add to replay buffer
-    
-    Params:
-        env (OpenAI Gym Environment)
-        behavior (Behavior)
-        buffer (ReplayBuffer)
-        n_episodes (int)
-        last_few (int):
-            how many episodes we use to calculate the desired return and horizon
-    '''
-    
     stochastic_policy = lambda state, command: behavior.action(state, command)
     
     for i in range(n_episodes_per_iter):
@@ -159,19 +92,6 @@ def generate_episodes(env, behavior, buffer, n_episodes, last_few):
 
 ### Utility Functions
 def initialize_replay_buffer(replay_size, n_episodes, last_few):
-    '''
-    Initialize replay buffer with warm-up episodes using random actions.
-    See section 2.3.1
-    
-    Params:
-        replay_size (int)
-        n_episodes (int)
-        last_few (int)
-    
-    Returns:
-        ReplayBuffer instance
-        
-    '''
     
     # This policy will generate random actions. Won't need state nor command
     random_policy = lambda state, command: np.random.randint(env.action_space.n)
@@ -191,20 +111,6 @@ def initialize_behavior_function(state_size,
                                  hidden_size, 
                                  learning_rate, 
                                  command_scale):
-    '''
-    Initialize the behaviour function. See section 2.3.2
-    
-    Params:
-        state_size (int)
-        action_size (int)
-        hidden_size (int) -- NOTE: not used at the moment
-        learning_rate (float)
-        command_scale (List of float)
-    
-    Returns:
-        Behavior instance
-    
-    '''
     
     behavior = Behavior(state_size, 
                         action_size, 
@@ -215,226 +121,127 @@ def initialize_behavior_function(state_size,
     
     return behavior
 
-
-
 ### Replay Buffer
-
 class ReplayBuffer():
-    '''
-    Replay buffer containing a fixed maximun number of trajectories with 
-    the highest returns seen so far
-    
-    Params:
-        size (int)
-    
-    Attrs:
-        size (int)
-        buffer (List of episodes)
-    '''
     
     def __init__(self, size=0):
         self.size = size
         self.buffer = []
         
     def add(self, episode):
-        '''
-        Params:
-            episode (namedtuple):
-                (states, actions, rewards, init_command, total_return, length)
-        '''
-        
         self.buffer.append(episode)
     
     def get(self, num):
-        '''
-        Params:
-            num (int):
-                get the last `num` episodes from the buffer
-        '''
-        
         return self.buffer[-num:]
     
     def random_batch(self, batch_size):
-        '''
-        Params:
-            batch_size (int)
-        
-        Returns:
-            Random batch of episodes from the buffer
-        '''
-        
         idxs = np.random.randint(0, len(self), batch_size)
         return [self.buffer[idx] for idx in idxs]
     
     def sort(self):
-        '''Keep the buffer sorted in ascending order by total return'''
-        
         key_sort = lambda episode: episode.total_return
         self.buffer = sorted(self.buffer, key=key_sort)[-self.size:]
     
     def save(self, filename):
-        '''Save the buffer in numpy format
-        
-        Param:
-            filename (str)
-        '''
-        
         np.save(filename, self.buffer)
     
     def load(self, filename):
-        '''Load a numpy format file
-        
-        Params:
-            filename (str)
-        '''
-        
         raw_buffer = np.load(filename)
         self.size = len(raw_buffer)
         self.buffer = \
-            [make_episode(episode[0], episode[1], episode[2], episode[3], episode[4], episode[5]) \
+            [(episode[0], episode[1], episode[2], episode[3], episode[4], episode[5]) \
              for episode in raw_buffer]
     
     def __len__(self):
-        '''
-        Returns:
-            Size of the buffer
-        '''
         return len(self.buffer)
 
-
 ### Behavior Function
-
 class Behavior(nn.Module):
-    '''
-    Behavour function that produces actions based on a state and command.
-    NOTE: At the moment I'm fixing the amount of units and layers.
-    TODO: Make hidden layers configurable.
-    
-    Params:
-        state_size (int)
-        action_size (int)
-        hidden_size (int) -- NOTE: not used at the moment
-        command_scale (List of float)
-    '''
     
     def __init__(self,  
                  aggregator, features=5, hidden_layer_size=5, embedding_size=10,
                  command_scale = [1, 1]):
 
-        ### TODO: Replace by Graph Neural Network
         super().__init__()
         
         self.command_scale = torch.FloatTensor(command_scale).to(device)
-               # Using Graph SAGE to get embedding using the neighbours
+        
+        # Using Graph SAGE to get embedding using the neighbours
         self.conv1 = SAGEConv(in_feats=features, out_feats=hidden_layer_size, aggregator_type=aggregator)
         self.conv2 = SAGEConv(in_feats=hidden_layer_size, out_feats=embedding_size, aggregator_type=aggregator)
+
+        # Use a sequential layer to get ecoding of the reward and command
         self.command_fn = nn.Sequential(nn.Linear(2, 10))
+
+        # Output function
+        self.output_nn = nn.Sequential(
+            nn.Linear(10, 32),
+            nn.Sigmoid(),
+
+            nn.Linear(32, 32),
+            nn.Sigmoid(),
+
+            nn.Linear(32, 32),
+            nn.Sigmoid(),
+
+            nn.Linear(32, 1)
+        )
         
         self.to(device)
         
-    
-    def forward(self, g, inputs, command):
-        '''Forward pass
-        
-        Params:
-            state (List of float)
-            command (List of float)
-        
-        Returns:
-            FloatTensor -- action logits
-        '''
-        h = inputs
-        h = self.conv1(g, h)
-        h = torch.sigmoid(h)
-        h = self.conv2(g, h)
-        h = torch.sigmoid(h)
-        h = self.conv3(g, h)
-        h = torch.sigmoid(h)
+    def get_gnn_embedding(self, g, inputs):
 
-        return h
-        state_output = self.state_fc(state)
-        command_output = self.command_fc(command * self.command_scale)
-        embedding = torch.mul(state_output, command_output)
-        return self.output_fc(embedding)
+        # Graph input to get embedding
+        graph = self.conv1(g, inputs)
+        graph = torch.sigmoid(graph)
+        graph = self.conv2(g, graph)
+
+        return torch.sigmoid(graph)
+
+    def forward(self, gnn_embeddings, command):
+        # Encode the the command values
+        command = self.command_fn(command)
+
+        # calculate product of command and graph-embedding
+        product = torch.mul(gnn_embeddings, command)
+
+        # get the output for the cmobination
+        return self.output_nn(product)
     
     def action(self, state, command):
-        '''
-        Params:
-            state (List of float)
-            command (List of float)
-            
-        Returns:
-            int -- stochastic action
-        '''
         
-        logits = self.forward(state, command)
-        probs = F.softmax(logits, dim=-1)
+        G, node_inputs, leaf_nodes = state
+        gnn_embeddings = self.get_gnn_embedding(G.to(device), node_inputs.to(device))
+        logits = self.forward(gnn_embeddings[leaf_nodes], command)
+        probs = F.softmax(logits)
         dist = Categorical(probs)
         return dist.sample().item()
     
     def greedy_action(self, state, command):
-        '''
-        Params:
-            state (List of float)
-            command (List of float)
-            
-        Returns:
-            int -- greedy action
-        '''
-        
-        logits = self.forward(state, command)
-        probs = F.softmax(logits, dim=-1)
+
+        G, node_inputs, leaf_nodes = state
+        gnn_embeddings = self.get_gnn_embedding(G.to(device), node_inputs.to(device))
+        logits = self.forward(gnn_embeddings[leaf_nodes], command)
+        probs = F.softmax(logits)
         return np.argmax(probs.detach().cpu().numpy())
     
     def init_optimizer(self, optim=Adam, lr=0.003):
-        '''Initialize GD optimizer
-        
-        Params:
-            optim (Optimizer) -- default Adam
-            lr (float) -- default 0.003
-        '''
-        
         self.optim = optim(self.parameters(), lr=lr)
     
     def save(self, filename):
-        '''Save the model's parameters
-        Param:
-            filename (str)
-        '''
-        
         torch.save(self.state_dict(), filename)
     
     def load(self, filename):
-        '''Load the model's parameters
-        
-        Params:
-            filename (str)
-        '''
-        
         self.load_state_dict(torch.load(filename))
 
-
 def UDRL(env, buffer=None, behavior=None, learning_history=[]):
-    '''
-    Upside-Down Reinforcement Learning main algrithm
-    
-    Params:
-        env (OpenAI Gym Environment)
-        buffer (ReplayBuffer):
-            if not passed in, new buffer is created
-        behavior (Behavior):
-            if not passed in, new behavior is created
-        learning_history (List of dict) -- default []
-    '''
-    
     if buffer is None:
         buffer = initialize_replay_buffer(replay_size, 
                                           n_warm_up_episodes, 
                                           last_few)
     
     if behavior is None:
-        behavior = initialize_behavior_function(state_size, 
-                                                action_size, 
+        behavior = initialize_behavior_function( 
                                                 hidden_size, 
                                                 learning_rate, 
                                                 [return_scale, horizon_scale])
